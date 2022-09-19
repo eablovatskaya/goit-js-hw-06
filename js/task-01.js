15db2b0968a22799b35501f7e5ba21da77c43071
eablovatskaya <ablovenok@gmail.com>
2022-09-19 21:55:45 +0300

    3

--- js/task-01.js
+++ js/task-01.js
@@ -1,11 +1,7 @@
 const categoriesEl = document.querySelectorAll(".item");
 console.log("Number of categories:", categoriesEl.length);
 
-const titleEl = document.querySelectorAll(".item h2");
-
-console.log("Category:", titleEl[0].textContent);
-console.log("Category:", categoriesEl[0].querySelectorAll("li").length);
-console.log("Category:", titleEl[1].textContent);
-console.log("Category:", categoriesEl[1].querySelectorAll("li").length);
-console.log("Category:", titleEl[2].textContent);
-console.log("Category:", categoriesEl[2].querySelectorAll("li").length);
+categoriesEl.forEach((item) => {
+  console.log(item.firstElementChild.textContent);
+  console.log(item.lastElementChild.querySelectorAll("li").length);
+});
